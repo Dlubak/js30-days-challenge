88d96416265d0a6a2c7fc37b0ae20496ba067ce3
Dlubak <jacekdlubak07@gmail.com>
2020-07-21 00:05:23 +0200

    add implemenetation for 2 challenge

--- 02 - JS and CSS Clock/clock.js
+++ 02 - JS and CSS Clock/clock.js
@@ -1,1 +1,20 @@
-console.log('it works');
+window.onload = function() {
+  const hourHand = document.querySelector('.hour-hand');
+  const minuteHand = document.querySelector('.min-hand');
+  const secondHand = document.querySelector('.second-hand');
+
+  function setClock() {
+    const date = new Date();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
+    const secondDeg = (second / 60) * 360 + 90;
+    const hourDeg = (hour / 12) * 360 + (minute / 60) * 30 + 90;
+    const minuteDeg = (minute / 60) * 360 + (second / 60) * 6 + 90;
+
+    hourHand.style.transform = `rotate(${hourDeg}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+    secondHand.style.transform = `rotate(${secondDeg}deg)`;
+  }
+  setInterval(setClock, 1000);
+};
