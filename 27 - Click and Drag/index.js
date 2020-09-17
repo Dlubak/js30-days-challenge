cc9a4b13240c96ea30582862b96402964e300e8e
Jacek Dlubak <jacek.dlubak@tieto.com>
2020-09-17 13:31:17 +0200

    27

---
+++ 27 - Click and Drag/index.js
@@ -0,0 +1,26 @@
+const slider = document.querySelector('.items');
+let mouseDown = false;
+let posX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', event => {
+  mouseDown = true;
+  slider.classList.add('active');
+  posX = event.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  mouseDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  mouseDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', event => {
+  if (!mouseDown) return;
+  event.preventDefault();
+  const x = event.pageX - slider.offsetLeft;
+  const drag = (x - posX) * 3;
+  slider.scrollLeft = scrollLeft - drag;
+});
