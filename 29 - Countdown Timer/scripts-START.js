1e1a7d03d1ccbdc1a2655798b6266907b15eff9f
Jacek Dlubak <jacek.dlubak@tieto.com>
2020-09-21 21:53:54 +0200

    29 start

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,18 +1,59 @@
 const timer = document.querySelector('.timer');
-const timerControls = timer.querySelectorAll('button');
-console.log(timerControls);
+const timerControls = timer.querySelectorAll('.timer__button');
+const timerForm = timer.querySelector('#custom');
 const display = document.querySelector('.display');
+let countdownTimer;
 
-function displayDate() {
-  const date = new Date();
-  const formattedDate = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-  display.querySelector('.display__time-left').innerHTML = formattedDate;
+function formatTimeLeft(date) {
+  const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+  const minutes =
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+  const seconds =
+    date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
+  return `${hours}:${minutes}:${seconds}`;
 }
-function calculateRemaingTime() {}
-// console.log(display.querySelector('.display__time-left'));
+
+function formatEndTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = (minutes * 60) % 60;
+  return `${minutes}:${seconds}`;
+}
+
+function displayTime(countdownTime) {
+  clearInterval(countdownTimer);
+  const timeNow = new Date();
+  const timeInMs = timeNow.getTime();
+  const endTime = new Date(timeInMs + countdownTime * 1000);
+  display.querySelector('.display__end-time').innerHTML = formatTimeLeft(
+    endTime
+  );
+  display.querySelector('.display__time-left').innerHTML = formatEndTime(
+    countdownTime
+  );
+  let timeLeft = countdownTime;
+  countdownTimer = setInterval(() => {
+    timeLeft -= 1;
+    display.querySelector('.display__time-left').innerHTML = formatEndTime(
+      timeLeft
+    );
+  }, 1000);
+  if (timeLeft <= 0) {
+    console.log('chuj');
+    clearInterval(countdownTimer);
+    alert('end');
+    return null;
+  }
+}
 function handleTimer(e) {
-  setInterval(displayDate, 100);
+  const countdownTime = e.currentTarget.dataset.time;
+  displayTime(countdownTime);
 }
 timerControls.forEach(timerButton =>
   timerButton.addEventListener('click', handleTimer)
 );
+timerForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const minutes = event.target.minutes.value;
+  displayTime(minutes * 60);
+  event.target.reset();
+});
